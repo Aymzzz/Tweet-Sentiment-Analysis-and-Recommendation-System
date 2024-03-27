614ab10c06a8f7c05292f27e0156db0958c79e42
Aymzzz <sbaiaymane6@gmail.com>
2024-03-27 23:41:51 +0000

    Updated consumer

--- kafka-con.py
+++ kafka-con.py
@@ -1,68 +1,33 @@
-from kafka import KafkaConsumer
 from neo4j import GraphDatabase, basic_auth
+from kafka import KafkaConsumer
 import json
-from textblob import TextBlob
 import logging
 
-#logging.basicConfig(level=logging.DEBUG)
-#logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s %(message)s',
+    level=logging.DEBUG
+)
 
-uri = "bolt://localhost:50585"
+uri = "bolt://localhost:64048"
 driver = GraphDatabase.driver(uri, auth=basic_auth("neo4j", "1234567890"))
 
-bootstrap_servers = ['localhost:9092']
-consumer = KafkaConsumer('twitter-text', bootstrap_servers=bootstrap_servers, auto_offset_reset='earliest')
+consumer = KafkaConsumer('twitter-sentiment', bootstrap_servers=['localhost:9092'])
 
-def perform_sentiment_analysis(text):
-    blob = TextBlob(text)
-    sentiment_score = blob.sentiment.polarity
-    return sentiment_score
-
-#try:
-for message in consumer:
+try:
+    for msg in consumer:
         try:
-            value = message.value.decode('utf-8')
-            tweet = json.loads(value)
-            text = tweet['text']
-            hashtags = tweet['hashtags']
-            usernames = tweet['usernames']
-    
-            # Perform sentiment analysis on the tweet content
-            sentiment_score = perform_sentiment_analysis(text)
-    
+            sentiment_data = json.loads(msg.value.decode('utf-8'))
+            sentiment = sentiment_data["sentiment"]
+            tweet_text = sentiment_data["text"]
+
             with driver.session() as session:
-                # Insert tweet into Neo4j
-                session.run(
-                    "MERGE (t:Tweet {text: $text}) SET t.sentiment = $sentiment",
-                    text=text, sentiment=sentiment_score
-                )
-    
-                if hashtags is not None:
-                    for hashtag in hashtags:
-                        # Create or update hashtag nodes in Neo4j
-                        session.run("MERGE (h:Hashtag {name: $name})", name=hashtag)
-                        # Create TAGGED relationships between tweet and hashtag
-                        session.run(
-                            "MATCH (t:Tweet {text: $text}), (h:Hashtag {name: $name}) MERGE (t)-[:TAGGED {name: 'TAGGED'}]->(h)",
-                            text=text, name=hashtag
-                        )
-    
-                if usernames is not None:
-                    for username in usernames:
-                        # Create or update user nodes in Neo4j
-                        session.run("MERGE (u:User {name: $name})", name=username)
-                        # Create MENTIONED relationships between tweet and user
-                        session.run(
-                            "MATCH (t:Tweet {text: $text}), (u:User {name: $name}) MERGE (t)-[:MENTIONED {name: 'MENTIONED'}]->(u)",
-                            text=text, name=username
-                        )
-            print((f"Processed tweet: {text}"))
-            #logger.info(f"Processed tweet: {text}")
-        except Exception as ex:
-            #logger.error(f"Error processing message: {str(ex)}")
-            print(f"Error processing message: {str(ex)}")
-#except Exception as e:
-    #logger.error(f"Error occurred: {str(e)}")
+                session.run("MATCH (t: Tweet {text: $text}) SET t.sentiment = $sentiment", text=tweet_text, sentiment=sentiment)
 
-
-driver.close()
+            logging.info(f"Updated sentiment for tweet text: {tweet_text}")
+        except Exception as e:
+            logging.error(f"Error processing message: {msg}. Error: {e}")
+except Exception as e:
+    logging.error(f"Error consuming messages from Kafka: {e}")
+finally:
+    consumer.close()
+    driver.close()
