cb1a8411002d0e9972cbd7f9d445df30c564f3fa
Aymzzz <sbaiaymane6@gmail.com>
2024-03-28 09:15:13 +0000

    Working recommendation sys

--- neo4j_utils.py
+++ neo4j_utils.py
@@ -5,22 +5,24 @@ driver = GraphDatabase.driver(uri, auth=basic_auth("neo4j", "1234567890"))
 
 def get_users_tweets_dict():
     with driver.session() as session:
-        result = session.run("MATCH (u:User)-[:POSTED]->(t:Tweet) RETURN u.name, collect(t.text) as tweets, collect(t.sentiment) as sentiments")
-    users_tweets = {}
-    for record in result:
-        user_id = record['u.name']
-        tweets = [tweet for tweet in record['tweets']]
-        sentiments = [sentiment for sentiment in record['sentiments']]
-        users_tweets[user_id] = (tweets, sentiments)
-    return users_tweets
+        result = session.run("MATCH (u:User)-[:POSTED]->(t:Tweet) RETURN u.name, collect(DISTINCT t.text) as tweets ORDER BY u.name")
+        users_tweets = {}
+        for record in result:
+            user = record['u.name']
+            tweets = record['tweets']
+            users_tweets[user] = tweets
+        # Consume the result
+        result.consume()
+        return users_tweets
 
 def get_hashtags_tweets_dict():
     with driver.session() as session:
-        result = session.run("MATCH (h:Hashtag)<-[:TAGGED]-(t:Tweet) RETURN h.name, collect(t.text) as tweets, collect(t.sentiment) as sentiments")
-    hashtags_tweets = {}
-    for record in result:
-        hashtag = record['h.name']
-        tweets = [tweet for tweet in record['tweets']]
-        sentiments = [sentiment for sentiment in record['sentiments']]
-        hashtags_tweets[hashtag] = (tweets, sentiments)
-    return hashtags_tweets
+        result = session.run("MATCH (h:Hashtag)<-[:TAGGED]-(t:Tweet) RETURN h.name, collect(DISTINCT t.text) as tweets ORDER BY h.name")
+        hashtags_tweets = {}
+        for record in result:
+            hashtag = record['h.name']
+            tweets = record['tweets']
+            hashtags_tweets[hashtag] = tweets
+        # Consume the result
+        result.consume()
+        return hashtags_tweets
--- recommendation-system.py
+++ recommendation-system.py
@@ -2,30 +2,36 @@ from neo4j_utils import get_users_tweets_dict, get_hashtags_tweets_dict
 from neo4j import GraphDatabase, basic_auth
 import logging
 
-# Import the users and hashtags tweets dictionaries
-users_tweets = get_users_tweets_dict()
-hashtags_tweets = get_hashtags_tweets_dict()
-
 # Initialize the driver
 uri = "bolt://localhost:64048"
 driver = GraphDatabase.driver(uri, auth=basic_auth("neo4j", "1234567890"))
 
-logging.basicConfig(
-    format='%(asctime)s %(levelname)s %(message)s',
-    level=logging.DEBUG
-)
+# Set the logging level to WARNING
+logging.basicConfig(level=logging.WARNING)
 
 def collaborative_filtering_recommendation(user_id):
     with driver.session() as session:
-        result = session.run("MATCH (u:User {name: $user_id})-[:POSTED]->(t:Tweet)-[:TAGGED]->(h:Hashtag)<-[:TAGGED]-(t2:Tweet)-[:POSTED]->(u2:User) WHERE u <> u2 WITH u2, count(DISTINCT h) as common_hashtags ORDER BY common_hashtags DESC RETURN u2.name, common_hashtags LIMIT 10", user_id=user_id)
-    return [(user, similarity) for user, similarity in result.records()]
+        result = session.run("MATCH (u:User {name: $user_id})-[:POSTED]->(t:Tweet)-[:TAGGED]->(h:Hashtag)<-[:TAGGED]-(t2:Tweet)-[:MENTIONED]->(u2:User) WHERE u <> u2 WITH u2, COUNT(DISTINCT h) AS common_hashtags ORDER BY common_hashtags DESC RETURN u2.name, common_hashtags LIMIT 10", user_id=user_id)
+        records = list(result)
+    return [(record['u2.name'], record['common_hashtags']) for record in records]
 
-def content_based_filtering_recommendation(hashtag):
+def content_based_filtering_recommendation(hashtag, hashtags_tweets):
     with driver.session() as session:
-        result = session.run("MATCH (h:Hashtag {name: $hashtag})<-[:TAGGED]-(t:Tweet) WHERE EXISTS(t.sentiment) RETURN t.text, t.sentiment ORDER BY t.sentiment DESC LIMIT 10", hashtag=hashtag)
-    return [(tweet, sentiment) for tweet, sentiment in result.records()]
+        result = session.run("MATCH (h:Hashtag {name: $hashtag})<-[:TAGGED]-(t:Tweet) WHERE t.sentiment IS NOT NULL RETURN t.text, t.sentiment ORDER BY t.sentiment DESC LIMIT 10", hashtag=hashtag)
+        records = list(result)
+    return [(record['t.text'], record['t.sentiment']) for record in records]
 
-# Testing it to see if it works
+if __name__ == '__main__':
 
-hashtag_recommendations = collaborative_filtering_recommendation("catvix")
-print(hashtag_recommendations)
+    users_tweets = get_users_tweets_dict()
+    hashtags_tweets = get_hashtags_tweets_dict()
+
+    result = None #we consume the result to avoid the ResultConsumedError
+
+    usrs_recommendations = collaborative_filtering_recommendation("catvix")
+    print(usrs_recommendations)
+    
+    print("================================================================================")
+    
+    tweet_recommendations = content_based_filtering_recommendation("#fb", hashtags_tweets)
+    print(tweet_recommendations)
